87417bb59557f6507dc7fd00c3f5e7c95dee9aaa
ASUS <pavanshetty05@gmail.com>
2023-02-18 13:33:58 +0530

    Final review

--- src/main/java/com/example/onlineShopping/OnlineShoppingApplication.java
+++ src/main/java/com/example/onlineShopping/OnlineShoppingApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class OnlineShoppingApplication {
 
 	public static void main(String[] args) {
+
+		System.out.println("Make the changes");
 		SpringApplication.run(OnlineShoppingApplication.class, args);
 	}
 
